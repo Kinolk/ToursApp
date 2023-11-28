2837b3f20e1a6bc559e731b3151a31e229ffeaa5
evgeniaoxogoeva@yandex.ru <140695894+Kinolk@users.noreply.github.com>
2023-11-28 23:16:49 +0800

    first commit

---
+++ ToursApp/ToursApp/AddEditPage.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ToursApp
+{
+    /// <summary>
+    /// Логика взаимодействия для AddEditPage.xaml
+    /// </summary>
+    public partial class AddEditPage : Page
+    {
+        //Текущий отель
+        private Hotel _currentHotel= new Hotel();
+        public AddEditPage(Hotel selectedHotel)
+        {
+            InitializeComponent();
+
+           
+
+
+            if (selectedHotel != null)
+                _currentHotel = selectedHotel;
+
+            DataContext= _currentHotel;
+            ComboCountries.ItemsSource = ToursAppEntities.GetContext().Country.ToList();
+        }
+
+        private void Btn_Save_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+
+            if (string.IsNullOrWhiteSpace(_currentHotel.Name))
+                errors.AppendLine("Укажите название отеля");
+            
+            if (_currentHotel.CountOfStars<1 || _currentHotel.CountOfStars>5)
+            {
+                errors.AppendLine("Количество звёзд - число от 1 до 5");
+            }
+            if (_currentHotel.Country == null)
+            {
+                errors.AppendLine("Выберите страну");
+            }
+            if(errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+
+            if (_currentHotel.id == 0)
+                ToursAppEntities.GetContext().Hotel.Add(_currentHotel);
+
+            try
+            {
+                ToursAppEntities.GetContext().SaveChanges();
+                MessageBox.Show("Информация сохранена!");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+            //Возращение на исходную страницу
+            NavigationService.GoBack();
+           
+        }
+    }
+}
---
+++ ToursApp/ToursApp/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace ToursApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// 
+    /// https://nationalteam.worldskills.ru/skills/rabota-s-bazoy-dannykh-v-prilozhenii-chtenie-dobavlenie-redaktirovanie-udalenie-dannykh-chast-1/
+    /// 6.Подтип
+    /// 
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainFrame.Navigate(new HotelsPage());
+            Manager.MainFrame = MainFrame;
+            //Manager.MainFrame.Navigate(new HotelsPage());
+          
+        }
+
+        private void Btn_Back_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.GoBack();
+        }
+
+        private void MainFrame_ContentRendered(object sender, EventArgs e)
+        {
+            if (MainFrame.CanGoBack)
+            {
+                Btn_Back.Visibility = Visibility.Visible;
+
+            }
+            else
+            {
+                Btn_Back.Visibility= Visibility.Hidden;
+            }
+        }
+    }
+}
